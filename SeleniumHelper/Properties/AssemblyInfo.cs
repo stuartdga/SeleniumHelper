df572fbf6169472ec5921ea70cddd5d8d50efe6f
stuartdga <stuartdga@yahoo.com>
2016-09-19 07:44:45 -0400

    Updated AssemblyInfo with version info

--- SeleniumHelper/Properties/AssemblyInfo.cs
+++ SeleniumHelper/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SeleniumHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple framework for creating Selenium tests using the Selenium WebDriver and the Selenium Grid")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Global Infrastructure Group")]
+[assembly: AssemblyCompany("Stuart Dickerson")]
 [assembly: AssemblyProduct("SeleniumHelper")]
-[assembly: AssemblyCopyright("Copyright © Global Infrastructure Group 2016")]
+[assembly: AssemblyCopyright("Copyright ©2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
